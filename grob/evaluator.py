3acd3c8fb4578bac7eff855624eac12ac7b8497d
Binyamin <binyaminf22@gmail.com>
2023-11-05 20:22:32 -0500

    order moves attempt

--- grob/evaluator.py
+++ grob/evaluator.py
@@ -14,6 +14,14 @@ INF = float("inf")
 
 
 def material_balance(board: chess.Board) -> float:
+    """
+
+    Args:
+        board:
+
+    Returns: the piece material balance of the board
+
+    """
     white_value = black_value = 0
     for piece_type in piece_values:
         white_value += len(board.pieces(piece_type, chess.WHITE)) * piece_values[piece_type]
@@ -26,21 +34,65 @@ def material_balance(board: chess.Board) -> float:
 
 
 def evaluate(board: chess.Board) -> float:
+    """
+
+    Args:
+        board:
+
+    Returns: board evaluation
+
+    """
     balance = material_balance(board)
     return balance
+
+
+def guess_move_evaluation(board: chess.Board, move: chess.Move) -> int:
+    """
+
+    Args:
+        board:
+        move:
+
+    Returns: guesses the evaluation of a move for move ordering
+
+    """
+    guess = 0
+    move_piece_type = board.piece_at(move.from_square)
+    capture_piece_type = board.piece_at(move.to_square)
+
+    # prioritize easy captures
+    if capture_piece_type is not None:
+        guess += 10 * piece_values[capture_piece_type] - piece_values[move_piece_type]
+
+    # prioritize promotions
+    if move.promotion is not None:
+        guess += piece_values[move.promotion]
+
+    # prioritize avoiding pawns
+    opposite_color = chess.WHITE if board.turn == chess.BLACK else chess.BLACK
+    attacking_pawns = board.attackers_mask(opposite_color, move.to_square) | board.pieces_mask(chess.PAWN, opposite_color)
+    if attacking_pawns != 0:
+        guess -= piece_values[move_piece_type]
+
+    return guess
+
+
+def order_moves(board: chess.Board, moves: list[chess.Move]):
+    moves.sort(key=lambda m: guess_move_evaluation(board, m), reverse=True)
 
 
 def search(board: chess.Board, depth: int, alpha: float, beta: float) -> float:
     if depth == 0:
         return evaluate(board)
 
-    moves = board.legal_moves
-    if moves.count() == 0:
+    moves = list(board.legal_moves)
+    if len(moves) == 0:
         if board.is_checkmate():
             return -INF  # current player has lost
         else:
             return 0     # game is a draw
 
+    order_moves(board, moves)
     for move in moves:
         board.push(move)
         evaluation = -search(board, depth - 1, -beta, -alpha)
@@ -54,9 +106,11 @@ def search(board: chess.Board, depth: int, alpha: float, beta: float) -> float:
 
 
 def next_move(board: chess.Board) -> chess.Move:
+    moves = list(board.legal_moves)
+    order_moves(board, moves)
     best_eval = -INF
     best_move = None
-    for move in board.legal_moves:
+    for move in moves:
         board.push(move)
         if (curr_eval := -search(board, 3, -INF, INF)) > best_eval:
             best_eval = curr_eval
