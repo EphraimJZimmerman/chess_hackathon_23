da7dfcb7da7d81290f9469c1965198b79a2fcdf4
Binyamin <binyaminf22@gmail.com>
2023-11-05 21:40:45 -0500

    move evaluation tests

--- grob/testing.py
+++ grob/testing.py
@@ -16,3 +16,13 @@ def test_obvious_black_win():
 def test_obvious_black_lose():
     obvious_black_lose = "8/1R6/8/8/8/8/2K5/k7 b - - 0 1"
     assert evaluation_from_fen(obvious_black_lose) < 10
+
+
+def test_guess_move_evaluation():
+    board = chess.Board("r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq d3 0 3")
+    knight_takes = chess.Move(chess.square(2, 5), chess.square(3, 3))
+    pawn_takes = chess.Move(chess.square(4, 4), chess.square(3, 3))
+    none_takes = chess.Move(chess.square(3, 6), chess.square(3, 4))
+    # prioritizes the correct captures
+    assert evaluator.guess_move_evaluation(board, pawn_takes) > evaluator.guess_move_evaluation(board, knight_takes)
+    assert evaluator.guess_move_evaluation(board, knight_takes) > evaluator.guess_move_evaluation(board, none_takes)
